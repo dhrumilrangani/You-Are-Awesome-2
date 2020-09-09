3a0d3b815cdb4c35bcdeb609fab0d2bd282c39f0
Dhrumil Rangani <rangani@bc.edu>
2020-09-08 20:01:02 -0400

    completed 2.9

--- You Are Awesome! 2/ViewController.swift
+++ You Are Awesome! 2/ViewController.swift
@@ -11,27 +11,38 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
+    var imageNumber = 0
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        messageLabel.text="Guess What?"
+        
     }
     
     @IBAction func messageButtonPressed(_ sender: Any) {
-        let messageAwesome = "You Are Awesome!"
-        let messageGreat = "You Are Great!"
-        let messageDaBomb = "You Are Da Bomb!"
-        
-        if messageLabel.text==messageAwesome {
-            messageLabel.text=messageGreat
-            imageView.image=UIImage(named: "image1")
-        } else if messageLabel.text==messageGreat {
-            messageLabel.text=messageDaBomb
-            imageView.image=UIImage(named: "image2")
-        } else {
-            messageLabel.text=messageAwesome
-            imageView.image=UIImage(named: "image0")
+        print(imageNumber)
+//        let imageName="image"+String(imageNumber)
+        let imageName="image\(imageNumber)"
+        imageView.image=UIImage(named: imageName)
+        imageNumber=imageNumber+1
+        if imageNumber==10 {
+            imageNumber=0
         }
+        
+//        let messageAwesome = "You Are Awesome!"
+//        let messageGreat = "You Are Great!"
+//        let messageDaBomb = "You Are Da Bomb!"
+//
+//        if messageLabel.text==messageAwesome {
+//            messageLabel.text=messageGreat
+//            imageView.image=UIImage(named: "image1")
+//        } else if messageLabel.text==messageGreat {
+//            messageLabel.text=messageDaBomb
+//            imageView.image=UIImage(named: "image2")
+//        } else {
+//            messageLabel.text=messageAwesome
+//            imageView.image=UIImage(named: "image0")
+//        }
     }
     
     
