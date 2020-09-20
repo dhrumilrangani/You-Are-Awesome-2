ac593b1408d055f39e7eed54e24a6157e1311d60
Dhrumil Rangani <rangani@bc.edu>
2020-09-19 22:42:34 -0400

    Completed 2.16

--- You Are Awesome! 2/ViewController.swift
+++ You Are Awesome! 2/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     var imageNumber = -1
     var messageNumber = -1
+    var soundNumber = -1
+    var totalSounds = 6
     var totalImages = 9
     var audioPlayer : AVAudioPlayer!
     
@@ -24,30 +26,8 @@ class ViewController: UIViewController {
         
     }
     
-    @IBAction func messageButtonPressed(_ sender: UIButton) {
-        print("Button was pressed")
-        
-        let message=["Act as if what you do makes a difference. It does.",
-                     "Success is not final, failure is not fatal: it is the courage to continue that counts.",
-                     "Never bend your head. Always hold it high. Look the world straight in the eye.",
-                     "Believe you can and you're halfway there."]
-        
-        var newMessageNumber: Int = Int.random(in: 0...message.count-1)
-        repeat {
-            newMessageNumber = Int.random(in: 0...message.count-1)
-        }while messageNumber == newMessageNumber
-        messageNumber = newMessageNumber
-        messageLabel.text = message[messageNumber]
-        
-   
-        var newImageNumber: Int = Int.random(in: 0...totalImages)
-        repeat {
-            newImageNumber = Int.random(in: 0...totalImages)
-        }while imageNumber == newImageNumber
-        imageNumber = newImageNumber
-        imageView.image = UIImage(named: "image\(imageNumber)")
-        
-        if let sound = NSDataAsset(name: "sound0"){
+    func playSound(name: String) {
+        if let sound = NSDataAsset(name: name){
             do {
                 try audioPlayer = AVAudioPlayer(data: sound.data)
                 audioPlayer.play()
@@ -56,6 +36,34 @@ class ViewController: UIViewController {
             }
         } else {
                 print("Error")
-}
+        }
+    }
+    
+    func nonRepeatingRandom(originalNumber: Int, upperlimit:Int) -> Int {
+        var newNumber: Int
+        repeat {
+            newNumber = Int.random(in: 0...upperlimit)
+        }while originalNumber == newNumber
+        return newNumber
+    }
+    
+    @IBAction func messageButtonPressed(_ sender: UIButton) {
+        print("Button was pressed")
+        
+        let message=["Act as if what you do makes a difference. It does.",
+                     "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+                     "Never bend your head. Always hold it high. Look the world straight in the eye.",
+                     "Believe you can and you're halfway there."]
+        
+        messageNumber = nonRepeatingRandom(originalNumber: messageNumber, upperlimit: message.count-1)
+        messageLabel.text = message[messageNumber]
+        
+   
+        imageNumber = nonRepeatingRandom(originalNumber: imageNumber, upperlimit: totalImages-1)
+        imageView.image = UIImage(named: "image\(imageNumber)")
+        
+        soundNumber  = nonRepeatingRandom(originalNumber: soundNumber, upperlimit: totalSounds-1)
+        playSound(name: "sound\(soundNumber)")
+        
 }
 }
